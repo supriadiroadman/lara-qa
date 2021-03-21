cec60464c3027c67c6c41cd00aa3a9482d175c9b
Supriadi <supriadiroadman@gmail.com>
2021-03-22 01:30:23 +0700

    Add votes, answers and views counter

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,12 +5,23 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
+                <div class="card-header text-white bg-success">All Questions</div>
 
-                <div class="card-body">
+                <div class="card-body bg-light">
                     @foreach ($questions as $question)
                     <div class="media">
-
+                        <div class="mr-5 text-center">
+                            <div class="mb-2">
+                                <h4 class="mb-0 font-weight-bold">{{ $question->votes }}</h4>
+                                <p>{{ Str::plural('vote', $question->votes) }}</p>
+                            </div>
+                            <div class="p-3 mb-3{{ $question->status }}">
+                                <h4 class="mb-0 font-weight-bold">{{ $question->answers }}</h4>
+                                <div>{{ Str::plural('answer', $question->answers) }}</div>
+                            </div>
+                            <div class="text-muted">
+                                {{ $question->views." ". \Str::plural('view', $question->views) }}</div>
+                        </div>
                         {{-- <img src="..." class="mr-3" alt="..."> --}}
                         <div class="media-body">
                             <h4 class="mt-0"><a href="{{ $question->url }}">{{$question->title}}</a></h4>
